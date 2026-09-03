9e78e7f2ca71fac24c3a6ba748102fc5809ee4f9
Luca Telini Crozara <gply714@gmail.com>
2026-09-03 23:34:14 +0000

    (03/09) Término da implementação do CRUD de clientes

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -4,6 +4,7 @@ import logger from 'morgan'
 
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
+import customersRouter from './routes/customers'
 
 const app = express()
 
@@ -12,7 +13,11 @@ app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
+/***************** ROTAS *************************/
+
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+
+app.use('/customers', customersRouter)
 
 export default app
